652caa20633ce65702fa955e8ec336fc22d7f38d
Max Zakharzhevskiy <maxzz@msn.com>
2021-05-05 15:02:30 -0700

    Add vite setup for production

---
+++ README.md
@@ -0,0 +1,7 @@
+#### About
+
+Experimanting with pseudo-3D shapes.
+
+#### Inspirations, references, credits
+
+[Cube generator](https://css-cube-generator.netlify.app/)
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [reactRefresh()]
-})
+export default (command: string) => defineConfig({
+    base: command === 'build' ? '/css-3d/': '/',
+    plugins: [reactRefresh()]
+});
