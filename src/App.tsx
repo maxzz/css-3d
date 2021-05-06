3ac702d70cb73983d305dc6ad8373cf36e83c6aa
Max Zakharzhevskiy <maxzz@msn.com>
2021-05-06 15:55:52 -0700

    Fix rerender issue with mouse rotation

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import './App.css';
 import MovementControl from './components/MovementControl';
 import CubeModel from './components/CubeModel';
 
 function useMouseRotate(active: boolean) {
-    const [rotation, setRotation] = useState({x: 0, y: 0});
+    const [rotation, setRotation] = useState({ x: 0, y: 0 });
 
     useEffect(() => {
         const handleMouse = (event: MouseEvent) => {
             setRotation({
                 x: (event.clientY - window.innerWidth / 2) / 5 * -1,
                 y: (event.clientX - window.innerWidth / 2) / 5,
-            })
+            });
         };
 
         active && window.addEventListener('mousemove', handleMouse, false);
@@ -27,9 +27,13 @@ function App() {
 
     console.log('aa', rotationActive, rotation);
 
+    let rotationCb = useCallback((v: boolean) => {
+        setRotationActive(v);
+    }, []);
+
     return (
         <div className="bg-gray-200 h-screen flex flex-col">
-            <MovementControl onClick={(v) => setRotationActive(v)} />
+            <MovementControl onClick={rotationCb} />
             <div className="flex-1 relative">
                 <CubeModel rotateX={rotation.x} rotateY={rotation.y} />
             </div>
