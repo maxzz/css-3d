40723fb03ac263804d4d1b6ea0e8a784fecb5140
Max Zakharzhevskiy <maxzz@msn.com>
2021-05-06 14:48:32 -0700

    Add hook mouse tracking

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,33 @@ import './App.css';
 import MovementControl from './components/MovementControl';
 import CubeModel from './components/CubeModel';
 
-function App() {
-    const [rotateX, setRotateX] = useState(140);
-    const [rotateY, setRotateY] = useState(20);
+function useMouseRotate({active}: {active: boolean}) {
+
+    const [rotation, setRotation] = useState({x: 0, y: 0});
 
     useEffect(() => {
         const handleMouse = (event: MouseEvent) => {
-            setRotateY((event.clientX - window.innerWidth / 2) / 5);
-            setRotateX((event.clientY - window.innerWidth / 2) / 5 * -1);
+            setRotation({
+                x: (event.clientY - window.innerWidth / 2) / 5 * -1,
+                y: (event.clientX - window.innerWidth / 2) / 5,
+            })
         };
         window.addEventListener('mousemove', handleMouse, false);
-        return () => {
-            window.removeEventListener('mousemove', handleMouse);
-        };
-    }, []);
+        return () => window.removeEventListener('mousemove', handleMouse);
+    }, [active]);
+
+    return rotation;
+}
+
+function App() {
+    let state = {active: true};
+    const rotation = useMouseRotate(state);
 
     return (
         <div className="bg-gray-200 h-screen flex flex-col">
             <MovementControl />
             <div className="flex-1 relative">
-                <CubeModel rotateX={rotateX} rotateY={rotateY} color="#1777" />
+                <CubeModel rotateX={rotation.x} rotateY={rotation.y} />
             </div>
         </div>
     );
