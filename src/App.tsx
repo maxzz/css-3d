371159e72fbeb98d99bd521b1fd779fc6107a9d6
Max Zakharzhevskiy <maxzz@msn.com>
2021-05-05 17:27:51 -0700

    Fix: wrong color separation

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,10 @@ function Cube() {
     let rotateX = 140;
     let rotateY = 20;
 
-    let colorValue = hexaToRgba('#1879da') || {r:0x18,g:0x79,b:0xda,a:1};
+    let colorValue = hexaToRgba('#1879da') || {r:0x18,g:0x79,b:0xda,a:1}; // #187979
     let red = colorValue.r;
     let green = colorValue.g;
-    let blue = colorValue.g;
+    let blue = colorValue.b;
     let opacity = colorValue.a;
 
     let shadowRatio = 5;
@@ -22,48 +22,42 @@ function Cube() {
         return `rgba(${red - n * shadowRatio}, ${green - n * shadowRatio}, ${blue - n * shadowRatio}, ${opacity})`;
     }
 
-    let style1 = //front
-    {
+    let style1 = { //front
         background: color(5),
         width: `${width}px`,
         height: `${height}px`,
         transform: `translate(-50%, -50%) rotateY(0deg) translateZ(${depth / 2}px)`
     };
 
-    let style2 = //left
-    {
+    let style2 = { //left
         background: color(10),
         width: `${depth}px`,
         height: `${height}px`,
         transform: `translate(-50%, -50%) rotateY(90deg) translateZ(${width / 2}px)`
     };
 
-    let style3 = //top
-    {
+    let style3 = { //top
         background: color(0),
         width: `${width}px`,
         height: `${depth}px`,
         transform: `translate(-50%, -50%) rotateX(90deg) translateZ(${height / 2}px)`
     };
 
-    let style4 = //bottom
-    {
+    let style4 = { //bottom
         background: color(20),
         width: `${width}px`,
         height: `${depth}px`,
         transform: `translate(-50%, -50%) rotateX(-90deg) translateZ(${height / 2}px)`
     };
 
-    let style5 = //right
-    {
+    let style5 = { //right
         background: color(10),
         width: `${depth}px`,
         height: `${height}px`,
         transform: `translate(-50%, -50%) rotateY(-90deg) translateZ(${width / 2}px)`
     };
 
-    let style6 = //back
-    {
+    let style6 = { //back
         background: color(15),
         width: `${width}px`,
         height: `${height}px`,
