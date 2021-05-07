df606298401aaeb6ea62a6ae1dbde8467ef329b4
Max Zakharzhevskiy <maxzz@msn.com>
2021-05-07 02:28:46 -0700

    WIP: playing with SVG path

--- src/components/LogoPlay.tsx
+++ src/components/LogoPlay.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import svgpath from 'svgpath';
 
 function LogoPlay() {
+    let path = "M13 10c0-7 9-10 23 0s18 0 26 0 29 20 0 21-19-11-30-16 1 16-10 22S7 35 7 23s6-6 6-13z";
+
+    let transformed = svgpath(path)
+                    .scale(0.5)
+                    .translate(10,20)
+                    .rel()
+                    .round(1)
+                    .toString();
+
     return (
         <svg viewBox="0 0 83 43">
             <filter filterUnits="objectBoundingBox" id="a">
@@ -12,7 +22,7 @@ function LogoPlay() {
                 </feMerge>
             </filter>
             <g filter="url(#a)">
-                <path d="M13 10c0-7 9-10 23 0s18 0 26 0 29 20 0 21-19-11-30-16 1 16-10 22S7 35 7 23s6-6 6-13z" fill="none" stroke="#f7931e" />
+                <path d={transformed} fill="none" stroke="#f7931e" />
             </g>
         </svg>
     );
