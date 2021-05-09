4ed61662fcdc2e75e8166b2f1ec4c51fbdb8bffb
Max Zakharzhevskiy <maxzz@msn.com>
2021-05-08 17:48:17 -0700

    WIP: remove Provider as useless so far

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,23 @@ import './App.css';
 import MovementControl from './components/MovementControl';
 import CubeView from './components/CubeView';
 import LogoPlay from './components/LogoPlay';
+import { Provider } from 'jotai';
 
 function App() {
     return (
-        <div className="bg-gray-200 h-screen flex flex-col">
-            <div className="flex">
-                <div className="w-32 pt-2">
-                    <LogoPlay />
+        // <Provider>
+            <div className="bg-gray-200 h-screen flex flex-col">
+                <div className="flex">
+                    <div className="w-32 pt-2">
+                        <LogoPlay />
+                    </div>
+                    <MovementControl />
                 </div>
-                <MovementControl />
+                <div className="flex-1 relative">
+                    <CubeView />
+                </div>
             </div>
-            <div className="flex-1 relative">
-                <CubeView />
-            </div>
-        </div>
+        // </Provider>
     );
 }
 
