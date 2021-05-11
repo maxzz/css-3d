63a5cdb2ef313e087382bbbb6a7963c5646f8cc5
Max Zakharzhevskiy <maxzz@msn.com>
2021-05-11 15:53:36 -0700

    Fix: update app header

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,12 @@ import { Provider } from 'jotai';
 function App() {
     return (
         <Provider>
-            <div className="bg-gray-200 h-screen flex flex-col">
-                <div className="flex">
+            <div className="bg-gray-100 h-screen flex flex-col">
+                <div className="flex items-center bg-gray-200 shadow-md">
                     <div className="w-32 pt-2">
                         <LogoPlay />
                     </div>
+                    <div className="ml-4 justify-self-center text-xl tracking-tighter uppercase text-pink-700">CSS 3D shapes</div>
                     <MovementControl />
                 </div>
                 <div className="flex-1 relative">
