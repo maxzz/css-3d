7c880d4a41600d6ff8cf8cf07d7ff2ecf05e898c
Max Zakharzhevskiy <maxzz@msn.com>
2021-05-12 01:58:21 -0700

    WIP: slider 2. add only specific styles

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,36 +1,46 @@
 import React from 'react';
 import '../assets/slider.scss';
 
+function mapRange(
+    value: number,
+    start1: number,
+    end1: number,
+    start2: number,
+    end2: number,
+): number {
+    const p = (value - start1) / (end1 - start1);
+    return start2 + p * (end2 - start2);
+}
+
 function Slider() {
     return (
-        <div className="tp-rotv">
-
-            <div className="tp-lblv tp-v-fst">
-                <div className="tp-lblv_l">spacing</div>
-                <div className="tp-lblv_v">
-                    <div className="tp-sldtxtv">
-                        <div className="tp-sldtxtv_s">
+        <div className="tp">
+            {/* Row */}
+            <div className="flex items-center">
+                <div className="tp-lblv">
+                    {/* Label */}
+                    <div className="tp-lblv_l">label</div>
+                    {/* Slider and Value container */}
+                    <div className="tp-lblv_v bg-purple-100">
+                        <div className="tp-sldtxtv">
+                            {/* Slider */}
                             <div className="tp-sldv">
                                 <div className="tp-sldv_t" tabIndex={0}>
-                                    <div className="tp-sldv_k" style={{ width: 0 }}>
+                                    b
+                                <div className="tp-sldv_k w-[67.35%]">
+                                        <div className="c">
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="tp-sldtxtv_t"><div className="tp-txtv tp-txtv-num">
-                            <input className="tp-txtv_i" type="text" />
-                            <div className="tp-txtv_k">
-                                <svg className="tp-txtv_g"><path className="tp-txtv_gb"></path><path className="tp-txtv_gh"></path>
-                                </svg>
-                                <div className="tp-ttv">
-                                </div>
+                            {/* Input Value */}
+                            <div className="tp-sldtxtv_t">
+                                <input type="text" value="32" />
                             </div>
-                        </div>
                         </div>
                     </div>
                 </div>
             </div>
-
         </div>
     );
 }
