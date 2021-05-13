4a820e4f398dc3b98548958d1383a44ae4a0c6ba
Max Zakharzhevskiy <maxzz@msn.com>
2021-05-12 23:42:42 -0700

    add connected jotai atoms

--- src/components/slider/SliderGroup.tsx
+++ src/components/slider/SliderGroup.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import SliderView, { NumberFormatter } from './SliderView';
 import './slider.scss';
+import { atom, useAtom } from 'jotai';
+
+const cubeW = atom(0);
+const cubeH = atom(0);
+const cubeD = atom(0);
 
 function SliderGroup() {
-    const [value, valueSet] = React.useState(40);
+    const [valueW, valueWSet] = useAtom(cubeW);
+    const [valueH, valueHSet] = useAtom(cubeH);
+    const [valueD, valueDSet] = useAtom(cubeD);
     const digits = 0;
     return (
         <div className="tp-vars tp-rotv custom-vars">
@@ -14,11 +21,11 @@ function SliderGroup() {
                     <div className="tp-lblv_v">
                         <div className="tp-sldtxtv">
                             <div className="tp-sldtxtv_s">
-                                <SliderView value={value} valueSet={valueSet} maxValue={50} />
+                                <SliderView value={valueW} valueSet={valueWSet} maxValue={50} />
                             </div>
                             <div className="tp-sldtxtv_t">
                                 <div className="tp-txtv tp-txtv-num">
-                                    <input className="tp-txtv_i" type="text" value={NumberFormatter(value, digits)} readOnly />
+                                    <input className="tp-txtv_i" type="text" value={NumberFormatter(valueW, digits)} readOnly />
                                 </div>
                             </div>
                         </div>
@@ -32,11 +39,29 @@ function SliderGroup() {
                     <div className="tp-lblv_v">
                         <div className="tp-sldtxtv">
                             <div className="tp-sldtxtv_s">
-                                <SliderView value={value} valueSet={valueSet} maxValue={50} />
+                                <SliderView value={valueH} valueSet={valueHSet} maxValue={50} />
                             </div>
                             <div className="tp-sldtxtv_t">
                                 <div className="tp-txtv tp-txtv-num">
-                                    <input className="tp-txtv_i" type="text" value={NumberFormatter(value, digits)} readOnly />
+                                    <input className="tp-txtv_i" type="text" value={NumberFormatter(valueH, digits)} readOnly />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* Row */}
+            <div className="flex items-center">
+                <div className="tp-lblv">
+                    <div className="tp-lblv_l">label</div>
+                    <div className="tp-lblv_v">
+                        <div className="tp-sldtxtv">
+                            <div className="tp-sldtxtv_s">
+                                <SliderView value={valueD} valueSet={valueDSet} maxValue={50} />
+                            </div>
+                            <div className="tp-sldtxtv_t">
+                                <div className="tp-txtv tp-txtv-num">
+                                    <input className="tp-txtv_i" type="text" value={NumberFormatter(valueD, digits)} readOnly />
                                 </div>
                             </div>
                         </div>
