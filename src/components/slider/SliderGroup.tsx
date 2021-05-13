4f8dffc1fbe510342ef901a33e3d6280469c3d6b
Max Zakharzhevskiy <maxzz@msn.com>
2021-05-12 18:06:20 -0700

    WIP: slider 13. add SliderView custom CSS vars. different way

--- src/components/slider/SliderGroup.tsx
+++ src/components/slider/SliderGroup.tsx
@@ -5,21 +5,19 @@ import './slider.scss';
 function SliderGroup() {
     const [value, valueSet] = React.useState(40);
     return (
-        <div className="tp-vars">
-            <div className="tp-rotv custom-vars">
-                {/* Row */}
-                <div className="flex items-center">
-                    <div className="tp-lblv">
-                        <div className="tp-lblv_l">label</div>
-                        <div className="tp-lblv_v">
-                            <div className="tp-sldtxtv">
-                                <div className="tp-sldtxtv_s">
-                                    <SliderView value={value} valueSet={valueSet} maxValue={100} />
-                                </div>
-                                <div className="tp-sldtxtv_t">
-                                    <div className="tp-txtv tp-txtv-num">
-                                        <input className="tp-txtv_i" type="text" value={NumberFormatter(value, 2)} readOnly />
-                                    </div>
+        <div className="tp-vars tp-rotv custom-vars">
+            {/* Row */}
+            <div className="flex items-center">
+                <div className="tp-lblv">
+                    <div className="tp-lblv_l">label</div>
+                    <div className="tp-lblv_v">
+                        <div className="tp-sldtxtv">
+                            <div className="tp-sldtxtv_s">
+                                <SliderView value={value} valueSet={valueSet} maxValue={100} />
+                            </div>
+                            <div className="tp-sldtxtv_t">
+                                <div className="tp-txtv tp-txtv-num">
+                                    <input className="tp-txtv_i" type="text" value={NumberFormatter(value, 2)} readOnly />
                                 </div>
                             </div>
                         </div>
