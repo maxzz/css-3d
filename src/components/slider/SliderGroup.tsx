94e6e49073e788854863b9744cae0e6e6d7a00a3
Max Zakharzhevskiy <maxzz@msn.com>
2021-05-14 14:27:20 -0700

    Fix: more strict string->number coercion

--- src/components/slider/SliderGroup.tsx
+++ src/components/slider/SliderGroup.tsx
@@ -4,10 +4,18 @@ import './slider.scss';
 import { useAtom } from 'jotai';
 import { cubeDAtom, cubeHAtom, cubeWAtom } from '../../atoms';
 
+const MAX_VALUE = 350;
+
 function SliderGroup() {
     const [valueW, valueWSet] = useAtom(cubeWAtom);
     const [valueH, valueHSet] = useAtom(cubeHAtom);
     const [valueD, valueDSet] = useAtom(cubeDAtom);
+
+    function getInputValue(ev: React.ChangeEvent<HTMLInputElement>, defValue: number, maxValue: number = MAX_VALUE) {
+        let v = parseInt(ev.target.value, 10);
+        v = isNaN(v) ? defValue : v;
+        return v;
+    }
 
     const digits = 0;
     return (
@@ -19,11 +27,11 @@ function SliderGroup() {
                     <div className="tp-lblv_v">
                         <div className="tp-sldtxtv">
                             <div className="tp-sldtxtv_s">
-                                <SliderView value={valueW} valueSet={valueWSet} maxValue={350} />
+                                <SliderView value={valueW} valueSet={valueWSet} maxValue={MAX_VALUE} />
                             </div>
                             <div className="tp-sldtxtv_t max-w-[2rem]">
                                 <div className="tp-txtv tp-txtv-num">
-                                    <input className="tp-txtv_i" type="text" value={NumberFormatter(valueW, digits)} onChange={(ev) => valueWSet(parseInt(ev.target.value, 10))} />
+                                    <input className="tp-txtv_i" type="text" value={NumberFormatter(valueW, digits)} onChange={(ev) => valueWSet(getInputValue(ev, valueW))} />
                                 </div>
                             </div>
                         </div>
@@ -37,11 +45,11 @@ function SliderGroup() {
                     <div className="tp-lblv_v">
                         <div className="tp-sldtxtv">
                             <div className="tp-sldtxtv_s">
-                                <SliderView value={valueH} valueSet={valueHSet} maxValue={350} />
+                                <SliderView value={valueH} valueSet={valueHSet} maxValue={MAX_VALUE} />
                             </div>
                             <div className="tp-sldtxtv_t max-w-[2rem]">
                                 <div className="tp-txtv tp-txtv-num">
-                                    <input className="tp-txtv_i" type="text" value={NumberFormatter(valueH, digits)} onChange={(ev) => valueHSet(parseInt(ev.target.value, 10))} />
+                                    <input className="tp-txtv_i" type="text" value={NumberFormatter(valueH, digits)} onChange={(ev) => valueHSet(getInputValue(ev, valueW))} />
                                 </div>
                             </div>
                         </div>
@@ -55,11 +63,11 @@ function SliderGroup() {
                     <div className="tp-lblv_v">
                         <div className="tp-sldtxtv">
                             <div className="tp-sldtxtv_s">
-                                <SliderView value={valueD} valueSet={valueDSet} maxValue={350} />
+                                <SliderView value={valueD} valueSet={valueDSet} maxValue={MAX_VALUE} />
                             </div>
                             <div className="tp-sldtxtv_t max-w-[2rem]">
                                 <div className="tp-txtv tp-txtv-num">
-                                    <input className="tp-txtv_i" type="text" value={NumberFormatter(valueD, digits)} onChange={(ev) => valueDSet(parseInt(ev.target.value, 10))} />
+                                    <input className="tp-txtv_i" type="text" value={NumberFormatter(valueD, digits)} onChange={(ev) => valueDSet(getInputValue(ev, valueW))} />
                                 </div>
                             </div>
                         </div>
