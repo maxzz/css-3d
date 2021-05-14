bd533fe8cbee961a71035829a7e09d7d4b44a978
Max Zakharzhevskiy <maxzz@msn.com>
2021-05-13 19:03:54 -0700

    Fix: cube model refactor for the code generation

--- src/components/CubeModel.tsx
+++ src/components/CubeModel.tsx
@@ -10,14 +10,13 @@ type CubeModelProps = {
     color?: string;
 };
 
-function CubeModel(props: CubeModelProps) {
+function getStyles(cubeProps: CubeModelProps) {
     const {
         width = 230,
         height = 350,
         depth = 100,
-        rotateX,
-        rotateY,
-        color} = props;
+        color,
+    } = cubeProps;
 
     let colorValue = hexaToRgba(color || '#1879da80') || { r: 0x18, g: 0x79, b: 0xda, a: 1 }; // #187979
     let red = colorValue.r;
@@ -31,62 +30,67 @@ function CubeModel(props: CubeModelProps) {
         return `rgba(${red - n * shadowRatio}, ${green - n * shadowRatio}, ${blue - n * shadowRatio}, ${opacity})`;
     }
 
-    let style1 = { //front
+    let f = { //front
         background: makeShade(5),
         width: `${width}px`,
         height: `${height}px`,
         transform: `translate(-50%, -50%) rotateY(0deg) translateZ(${depth / 2}px)`
     };
 
-    let style2 = { //left
+    let l = { //left
         background: makeShade(10),
         width: `${depth}px`,
         height: `${height}px`,
         transform: `translate(-50%, -50%) rotateY(90deg) translateZ(${width / 2}px)`
     };
 
-    let style3 = { //top
+    let t = { //top
         background: makeShade(0),
         width: `${width}px`,
         height: `${depth}px`,
         transform: `translate(-50%, -50%) rotateX(90deg) translateZ(${height / 2}px)`
     };
 
-    let style4 = { //bottom
+    let b = { //bottom
         background: makeShade(20),
         width: `${width}px`,
         height: `${depth}px`,
         transform: `translate(-50%, -50%) rotateX(-90deg) translateZ(${height / 2}px)`
     };
 
-    let style5 = { //right
+    let r = { //right
         background: makeShade(10),
         width: `${depth}px`,
         height: `${height}px`,
         transform: `translate(-50%, -50%) rotateY(-90deg) translateZ(${width / 2}px)`
     };
 
-    let style6 = { //back
+    let k = { //back
         background: makeShade(15),
         width: `${width}px`,
         height: `${height}px`,
         transform: `translate(-50%, -50%) rotateY(180deg) translateZ(${depth / 2}px)`
     };
 
+    return {f, l, t, b, r, k};
+}
+
+function CubeModel(props: CubeModelProps) {
+    const styles = getStyles(props)
     return (
         <div
             className="absolute top-1/2 left-1/2 select-none"
-            style={{ transformStyle: 'preserve-3d', transform: `rotateX(${rotateX}deg) rotateY(${rotateY}deg)` }}
+            style={{ transformStyle: 'preserve-3d', transform: `rotateX(${props.rotateX}deg) rotateY(${props.rotateY}deg)` }}
         >
-            <div className="absolute" style={style1}></div> {/* _front_ */}
-            <div className="absolute" style={style2}></div> {/* _left__ */}
-            <div className="absolute" style={style3}></div> {/* _top___ */}
-            <div className="absolute" style={style4}></div> {/* _bottom */}
-            <div className="absolute" style={style5}></div> {/* _right_ */}
-            <div className="absolute" style={style6}></div> {/* _back__ */}
+            <div className="absolute" style={styles.f}></div> {/* _front_ */}
+            <div className="absolute" style={styles.l}></div> {/* _left__ */}
+            <div className="absolute" style={styles.t}></div> {/* _top___ */}
+            <div className="absolute" style={styles.b}></div> {/* _bottom */}
+            <div className="absolute" style={styles.r}></div> {/* _right_ */}
+            <div className="absolute" style={styles.k}></div> {/* _back__ */}
 
-            <div className="absolute" style={{...style5, background: 'transparent', borderBottom: '3px solid blue', borderLeft: '3px solid green'}}></div> {/* _right_ */}
-            <div className="absolute" style={{...style6, background: 'transparent', borderBottom: '3px solid red'}}></div> {/* _back__ */}
+            <div className="absolute" style={{...styles.r, background: 'transparent', borderBottom: '3px solid blue', borderLeft: '3px solid green'}}></div> {/* _right_ */}
+            <div className="absolute" style={{...styles.k, background: 'transparent', borderBottom: '3px solid red'}}></div> {/* _back__ */}
         </div>
     );
 }
