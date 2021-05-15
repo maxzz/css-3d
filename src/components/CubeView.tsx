11ab9c1734c460edf27de6b8016865976ad2892a
Max Zakharzhevskiy <maxzz@msn.com>
2021-05-14 20:22:25 -0700

    Feature: add source code dark background

--- src/components/CubeView.tsx
+++ src/components/CubeView.tsx
@@ -34,13 +34,15 @@ function CubeView() {
 
     return (
         <div className="relative h-full">
-            <CubeModel rotateX={rotation.x} rotateY={rotation.y} width={width} height={height} depth={depth} />
-            {/* <div className="px-4">x:{rotation.x} y:{rotation.y}</div> */}
-            <div className="absolute top-0 left-0 text-[.5rem] font-mono text-green-700">
+            {showGenerated && <div className="absolute top-0 left-0 text-[.5rem] font-mono text-green-700 bg-gray-900 w-full h-full">
                 <pre>
-                    {showGenerated && showSource()}
+                    {showSource()}
                 </pre>
+            </div>}
+            <div className="z-10">
+                <CubeModel rotateX={rotation.x} rotateY={rotation.y} width={width} height={height} depth={depth} />
             </div>
+            {/* <div className="px-4">x:{rotation.x} y:{rotation.y}</div> */}
         </div>
     );
 }
