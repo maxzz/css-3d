1ffef7d6d2ef77f9f4a96ecbee425b1115f5ac91
Max Zakharzhevskiy <maxzz@msn.com>
2021-05-14 19:53:35 -0700

    WIP: update logo SVG. 2

--- src/components/LogoPlay.tsx
+++ src/components/LogoPlay.tsx
@@ -11,15 +11,15 @@ function LogoPlay() {
         }).scale(1.05);
 
     return (
-        <svg viewBox="0 0 94.2 81.9" className="w-full h-full">
+        <svg viewBox="0 0 94.2 81.9" className="w-full h-full" strokeWidth="2">
             <linearGradient id="SVGID_1_" gradientUnits="userSpaceOnUse" x1="15" y1="5.3" x2="70.3" y2="83.4">
-                <stop offset="0" stopColor="#3a15ff" />
-                <stop offset="1" stopColor="#fff" />
+                <stop offset="0" stopColor="rgb(185, 28, 28)" />
+                <stop offset="1" stopColor="rgba(185, 28, 28, .3)" />
             </linearGradient>
             <g fill="none" stroke="url(#SVGID_1_)">
                 <path d="M14.1 23.1C8.9 6.8 23.3-9.7 44.1 8.4c14.8 12.9 8-5.5 19-5.5s45 55.5 24 50.5c-27.3-6.5 9.6 26.3-20.4 28C44.2 82.5 16.9 48.5 18 68.1 19.5 92.7.5 71.7.5 47s17.9-10.6 13.6-23.9z" />
-                <path d="M24 19.6c.7 2.2 2 6 2.9 2.8.9-3.2.1-5.5-1.9-7.2-2-1.8-1.7 2.1-1 4.4z" />
-                <path d="M30.2 16c-1.1-1.6-2.8-4.5-.1-3.9s3.7 2.4 3.7 4.9c-.2 2.7-2.6.6-3.6-1z" />
+                <path d="M24 19.6c.7 2.2 2 6 2.9 2.8.9-3.2.1-5.5-1.9-7.2-2-1.8-1.7 2.1-1 4.4z" strokeWidth=".9"/>
+                <path d="M30.2 16c-1.1-1.6-2.8-4.5-.1-3.9s3.7 2.4 3.7 4.9c-.2 2.7-2.6.6-3.6-1z" strokeWidth=".9"/>
             </g>
         </svg>
 
