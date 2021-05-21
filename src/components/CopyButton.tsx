5072921d3be83a38c4ae9e042c1de7c7f57de524
Max Zakharzhevskiy <maxzz@msn.com>
2021-05-21 00:22:16 -0700

    Feature: copy-clipboard. fix jsx-native-events

--- src/components/CopyButton.tsx
+++ src/components/CopyButton.tsx
@@ -3,13 +3,22 @@ import React from 'react';
 import '@github/clipboard-copy-element';
 import nativeEvents from 'jsx-native-events';
 
-function CopyButton(props: {className?: string; forId: string}) {
+function CopyButton(props: { className?: string; forId: string; }) {
     const {
         className = "",
         forId,
     } = props;
     return (
-        <clipboard-copy class={className} for={forId} onClipboardCopy={() => {console.log('aaa')}}>
+        <clipboard-copy class={className} for={forId} onEventClipboardCopy={(event: Event) => {
+
+            const notice = (event.target! as HTMLElement).querySelector('.notice') as HTMLElement;
+            notice.hidden = false;
+            // setTimeout(function () {
+            //     notice.hidden = true;
+            // }, 1000);
+
+            console.log('aaa', event);
+        }}>
             Copy
             <span className="notice" hidden>Copied!</span>
         </clipboard-copy>
--- src/types.d.ts
+++ src/types.d.ts
@@ -6,7 +6,13 @@ declare global {
             'clipboard-copy': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement> & {
                 class: string;
                 for: string;
+                onEventClipboardCopy: (event: Event) => void
             }, HTMLElement>;
         }
     }
 }
+
+// declare module 'jsx-native-events' {
+//     // import nativeEvents from 'jsx-native-events';
+//     // export default nativeEvents;    
+// }
