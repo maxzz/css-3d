2cafece28a1cfa28050057518dd400bf4b7161fc
Max Zakharzhevskiy <maxzz@msn.com>
2021-05-22 16:03:18 -0700

    Fix: move code generator to a separate place

---
+++ src/utils/text-generator-legacy.ts
@@ -0,0 +1,39 @@
+import { CubeStyles } from '../components/CubeModel';
+
+function objectToCss<T extends object>(obj: Exclude<T, any[] | Function>): string {
+    return JSON.stringify(obj, null, 4)
+        .replace(/"/g, '')
+        .replace(/,$/mg, ';')
+        .replace(/((?:\r?\n)\s*)};?/mg, ';$1}')
+        .replace(/};((?:\r?\n)\s*)}/mg, '}$1}');
+}
+
+export function lagacyGenerator(styles: CubeStyles, rotation: {x: number; y: number}): string {
+    /* CSS */
+    let parent = `.cube ${objectToCss({
+        'transform-style': 'preserve-3d',
+        transform: `rotateX(${rotation.x}deg) rotateY(${rotation.y}deg)`
+    })}`;
+
+    let face = `.cube__face ${objectToCss({
+        position: 'absolute',
+    })}`;
+
+    let faces = [...Object.entries(styles)].map(([key, value]) => `.cube__${key} ${objectToCss(value)}`).join('\n');
+
+    /* HTML */
+
+    let html = `
+        /* HTML */
+
+        <div class="cube">
+            <div class="cube__face cube__f"></div> <!-- _front_ -->
+            <div class="cube__face cube__l"></div> <!-- _left__ -->
+            <div class="cube__face cube__t"></div> <!-- _top___ -->
+            <div class="cube__face cube__b"></div> <!-- _bottom -->
+            <div class="cube__face cube__r"></div> <!-- _right_ -->
+            <div class="cube__face cube__k"></div> <!-- _back__ -->
+        </div>`.replace(/^[ \t]{12,13}/gm, '');
+
+    return `/* CSS */\n\n${parent}\n\n${face}\n\n${faces}\n${html}`;
+}
