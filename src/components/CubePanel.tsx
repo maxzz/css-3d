c9e7a4ea66e867e9c33689413ff8207f5c5c0e35
Max Zakharzhevskiy <maxzz@msn.com>
2021-05-23 14:27:03 -0700

    WIP: play w/ axes. 7. tailwind has no rotation around axes

--- src/components/CubePanel.tsx
+++ src/components/CubePanel.tsx
@@ -28,35 +28,41 @@ function CubeView() {
 
     return (
         <div className="relative h-full pointer-events-none">
+            {/* Generated text */}
             {showGenerated && <div className="absolute top-0 left-0 text-[.5rem] font-mono text-green-700 bg-gray-900 w-full h-full">
                 <CopyButton forId="generated-source" className="absolute top-2 right-2 px-2 py-1 rounded-sm border border-green-700 " />
                 <pre id="generated-source" className="py-1">
                     {showSource()}
                 </pre>
             </div>}
-            {/* Mini cube */}
+
+            {/* Cube and Axes */}
             <div className="w-[400px] h-[400px] ml-auto">
+                {/* Mini cube */}
                 <div className={`z-10 absolute select-none transform ${showGenerated ? 'top-32 right-28 scale-[.2]' : 'top-1/2 right-1/2 scale-[.8]'}`}>
                     <CubeModel angle={angle} width={width} height={height} depth={depth} color={color} shadowRatio={shadowRatio} />
                 </div>
-
-                    <div className="w-full h-full transform scale-[.2]">
-                        {/* x */}
-                        <div className="transform translate-x-[250px] translate-y-[-250px]">
-                            <CubeModel angle={angle} width={500} height={10} depth={500} color={{r: 0xff, g: 0, b: 0, a: 0.1}} shadowRatio={shadowRatio} />
-                        </div>
-                        {/* y */}
-                        <div className="transform translate-x-[0px] translate-y-[-500px]">
-                            <CubeModel angle={angle} width={10} height={500} depth={500} color={{r: 0, g: 0xff, b: 0, a: 0.1}} shadowRatio={shadowRatio} />
-                        </div>
-                        {/* z */}
-                        <div className="transform translate-x-[250px] translate-y-[-250px]">
-                            <CubeModel angle={angle} width={500} height={500} depth={10} color={{r: 0, g: 0, b: 0xff, a: 0.1}} shadowRatio={shadowRatio} />
-                        </div>
-                        {/* style={{transformOrigin: '50% 100%'}} */}
-                        {/* style={{'--tw-translate-x': '25%'} as React.CSSProperties } */}
+{/* transform translate-x-[250px] translate-y-[-250px] */}
+{/*  */}
+                {/* Axes */}
+                <div className={`w-full h-full transform scale-[.2] rotate-x-[${angle.x}deg] rotate-y-[${angle.y}deg]`} style={{transformStyle: 'preserve-3d'}}>
+                    {/* x: red */}
+                    <div className="" style={{transform: 'translateX(250px) translateZ(-250px)'}}>
+                        <CubeModel angle={{x: 0, y: 0}} width={500} height={10} depth={10} color={{r: 0xff, g: 0, b: 0, a: 0.1}} shadowRatio={shadowRatio} />
                     </div>
-
+                    {/* y: green */}
+                    {/* transform translate-x-[0px] translate-y-[-500px] */}
+                    <div className="" style={{transform: 'translateY(-250px)'}}>
+                        <CubeModel angle={{x: 0, y: 0}} width={10} height={500} depth={10} color={{r: 0, g: 0xff, b: 0, a: 0.1}} shadowRatio={shadowRatio} />
+                    </div>
+                    {/* z: blue */}
+                    {/* transform translate-x-[250px] translate-y-[-250px] */}
+                    <div className="" style={{transform: 'translateZ(250px)'}}>
+                        <CubeModel angle={{x: 0, y: 0}} width={10} height={10} depth={500} color={{r: 0, g: 0, b: 0xff, a: 0.1}} shadowRatio={shadowRatio} />
+                    </div>
+                    {/* style={{transformOrigin: '50% 100%'}} */}
+                    {/* style={{'--tw-translate-x': '25%'} as React.CSSProperties } */}
+                </div>
             </div>
         </div>
     );
