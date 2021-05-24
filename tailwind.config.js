5911f427022f56fe1111bcb679e8caf5cf53f271
Max Zakharzhevskiy <maxzz@msn.com>
2021-05-23 17:03:55 -0700

    Feature: add tailwind radial-gradients

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ module.exports = {
         extend: {
             colors: {
                 'dark-label': '#6b7280' //var(--tp-label-foreground-color)
-            }
+            },
+            backgroundImage: () => ({
+                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+                'gradient-radial-to-tr': 'radial-gradient(115% 90% at 0% 100%, var(--tw-gradient-stops))',
+                'gradient-radial-to-tl': 'radial-gradient(115% 90% at 100% 100%, var(--tw-gradient-stops))',
+                'gradient-radial-to-br': 'radial-gradient(90% 115% at 0% 0%, var(--tw-gradient-stops))',
+                'gradient-radial-to-bl': 'radial-gradient(90% 115% at 100% 0%, var(--tw-gradient-stops))',                
+            })
         },
     },
     variants: {
